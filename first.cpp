825bbb5aef26fa8609a17821b3e23c74d40a8143
Piotrek <piotrbelkner@gmail.com>
2017-03-31 17:05:43 -0700

    Gnuplot was added. It works nicely. README updated with command to compile second.

--- first.cpp
+++ first.cpp
@@ -10,6 +10,9 @@
 
 //void  SIGINT_handler(int);   
 void  SIGQUIT_handler(int); 
+void writeDataToMemory(std::vector<int> data, pid_t * pointerToMemory);
+void signal_received(int sig, siginfo_t *info, void *context);
+void prepareData(std::vector<int> &data, int sizeOfData);
 
 int   ShmID;      
 pid_t *ShmPTR;    
@@ -22,16 +25,72 @@ bool shouldDataBePrepared = false;
 #define SHMSZ  100
 #define KEY_VALUE 5678
 
+
+
+    
+int main(void)
+{
+  pid_t pid = getpid();
+  std::vector<int> data;
+  key_t MyKey;
+  MyKey = KEY_VALUE; 
+
+  //Prepare response for signals
+  struct sigaction act;
+  act.sa_sigaction = &signal_received;
+  act.sa_flags = SA_SIGINFO;
+
+
+  sigaction(SIGINT, &act, NULL);
+  if (signal(SIGQUIT, SIGQUIT_handler) == SIG_ERR) 
+  {
+    printf("SIGQUIT install error\n");
+    exit(2);
+  }
+  //Prepare memory and write pid into it
+  ShmID   = shmget(MyKey, SHMSZ, IPC_CREAT | 0666);
+  ShmPTR  = (int *) shmat(ShmID, NULL, 0);
+  *ShmPTR = pid; 
+
+  for (;;) 
+  {        
+    std::cout << "Waiting for signal from another process.\n"; //To be deleted
+    if(firstSignal == false)
+    {
+      if(shouldDataBePrepared == true)
+      {	
+        prepareData(data, SHMSZ);
+	writeDataToMemory(data, ShmPTR);
+	kill(anotherProcessPid, SIGINT);
+	shouldDataBePrepared = false;
+      }
+				
+     }
+     sleep(3); //To be deleted 
+    }
+
+return 1;
+}
+
+void  SIGQUIT_handler(int sig)
+{
+  signal(sig, SIG_IGN);
+  printf("From SIGQUIT: just got a %d (SIGQUIT ^\\) signal and is about to quit\n", sig);
+  shmdt(ShmPTR);
+  shmctl(ShmID, IPC_RMID, NULL);
+  exit(3);
+}
+
 void signal_received(int sig, siginfo_t *info, void *context) 
 {
   if(firstSignal == true) 
-	{ 
- 	  std::cout <<"Paring completed\n";
+  { 
+    std::cout <<"Paring completed\n";
     anotherProcessPid = info->si_pid;
-		firstSignal = false;
+    firstSignal = false;
   }
-	shouldDataBePrepared = true;
-	multipl ++;
+  shouldDataBePrepared = true;
+  multipl ++;
 }
 
 void writeDataToMemory(std::vector<int> data, pid_t * pointerToMemory)
@@ -41,11 +100,11 @@ void writeDataToMemory(std::vector<int> data, pid_t * pointerToMemory)
 	s = (int *)pointerToMemory;  
 	*s = data.size();
 
-	std::cout <<"s: " << s << "\n"; //To be deleted
-	std::cout <<"data.size(): " << data.size() << "\n"; //To be deleted
+	//std::cout <<"s: " << s << "\n"; //To be deleted
+	//std::cout <<"data.size(): " << data.size() << "\n"; //To be deleted
 	for (int c = 0; c <= data.size(); c++)
 	{ 
-		*++s = data[c];
+	  *++s = data[c];
 	}
 	 *s = NULL;
 }
@@ -54,62 +113,10 @@ void prepareData(std::vector<int> &data, int sizeOfData)//funktor na jakas funkc
 {
   data.clear();
   for(int i =0 ;i <=sizeOfData; i++)
-	{
-		data.push_back(i*multipl);
-	}
+  {
+    data.push_back(i*multipl);
+  }
 }
 
-    
-int main(void)
-{
-    pid_t pid = getpid();
-	  std::vector<int> data;
-    key_t MyKey;
-		MyKey = KEY_VALUE; 
 
-		//Prepare response for signals
-		struct sigaction act;
-    act.sa_sigaction = &signal_received;
-    act.sa_flags = SA_SIGINFO;
-
-
-    sigaction(SIGINT, &act, NULL);
-    if (signal(SIGQUIT, SIGQUIT_handler) == SIG_ERR) 
-    {
-        printf("SIGQUIT install error\n");
-        exit(2);
-    }
-    //Prepare memory and write pid into it
- 	  ShmID   = shmget(MyKey, SHMSZ, IPC_CREAT | 0666);
-    ShmPTR  = (int *) shmat(ShmID, NULL, 0);
-		*ShmPTR = pid; 
-
-    for (;;) 
-		{        
-	 		std::cout << "Waiting for signal from another process.\n"; //To be deleted
-			if(firstSignal == false)
-			{
-				if(shouldDataBePrepared == true)
-				{	
-				  prepareData(data, SHMSZ);
-					writeDataToMemory(data, ShmPTR);
-			  	kill(anotherProcessPid, SIGINT);
-					shouldDataBePrepared = false;
-				}
-				
-			}
-        sleep(3); //To be deleted 
-    }
-
-return 1;
-}
-
-void  SIGQUIT_handler(int sig)
-{
-     signal(sig, SIG_IGN);
-     printf("From SIGQUIT: just got a %d (SIGQUIT ^\\) signal and is about to quit\n", sig);
-     shmdt(ShmPTR);
-     shmctl(ShmID, IPC_RMID, NULL);
-     exit(3);
-}
 
--- second.cpp
+++ second.cpp
@@ -1,17 +1,28 @@
 #include  <stdio.h>
 #include  <sys/types.h>
-#include  <signal.h>
 #include  <sys/ipc.h>
 #include  <sys/shm.h>
+#include  <signal.h>
 #include  <unistd.h>
 #include  <stdlib.h>  
 #include  <iostream>
+#include  <fstream>
+#include  <vector>
+#include  <map>
+#include  <limits>
+#include  <cmath>
+#include  <cstdio>
+#include  <boost/tuple/tuple.hpp>
+#include  <boost/foreach.hpp>
+#include "gnuplot-iostream.h"
 
 #define SHMSZ     100
 #define KEY_VALUE 5678
 
-int *ShmPTR;
 void  SIGINT_handler(int sig);
+void plotChart(std::vector<int> valuesToPlot, double maxX =1, double step =0.01);
+
+int *ShmPTR;
 
 int main(void)
 {
@@ -27,27 +38,27 @@ int main(void)
   MyKey = KEY_VALUE;
   int  ShmID;
   char  line[SHMSZ], c;
+  bool first = true;
 
   ShmID   = shmget(MyKey, SHMSZ, 0666);
   ShmPTR  = (int *) shmat(ShmID, NULL, 0);
   pid     = *ShmPTR;                
  
-  bool first = true;
   while (1) 
   {      
-   // std::cout << std::flush;                 
+
     printf("Want to interrupt  or kill another process (i or k)? \n");
-   // std::cout << std::flush;
     fgets(line, sizeof(line), stdin);
     c = line[0];
+
     if(c == 'i' || c == 'I')
-		{  
+    {  
        kill(pid, SIGINT);
        printf("Sent a SIGINT signal\n"); 
- 			 sleep(1);   //Wait a second for signal to be received. TB deleted                  					 
+       sleep(1);   //Wait a second for signal to be received. TB deleted                  					 
     }
     else if (c == 'k' || c == 'K') 
-		{
+    {
        printf("About to send a SIGQUIT signal\n");
        kill(pid, SIGQUIT);     
        printf("Done.....\n");
@@ -56,6 +67,7 @@ int main(void)
     else
       printf("Wrong keypress (%c).  Try again\n", c);
   }
+
 return 1;
 }
 
@@ -65,22 +77,43 @@ void printData(int * pointerToMemory)
   int *s, i=0;
   s = pointerToMemory;
   std::cout <<"s: " << s << "\n"; //To be deleted
-	int amountOfData = *s;
-	std::cout <<"amountOfData: " << amountOfData << "\n";  //To be deleted
-	while(i != amountOfData+1)
-	{
-		std::cout<< "*s: " << (*s) << "\n" ;  //To be deleted
-		i++;
-		s++;
-	}
+  int amountOfData = *s;
+  std::cout <<"amountOfData: " << amountOfData << "\n";  //To be deleted
+  std::vector<int> data;
+  while(i != amountOfData+1)
+  {
+    std::cout<< "*s: " << (*s) << "\n" ;  //To be deleted
+    data.push_back(*s);
+    i++;
+    s++;
+  }
+  plotChart(data);
 }
 
 void  SIGINT_handler(int sig)
 {
-	signal(sig, SIG_IGN);
-	printf("From SIGINT: just got a %d (SIGINT ^C) signal\n", sig);
-	printData(ShmPTR);
-	signal(sig, SIGINT_handler);
+  signal(sig, SIG_IGN);
+  printf("From SIGINT: just got a %d (SIGINT ^C) signal\n", sig);
+  printData(ShmPTR);
+  signal(sig, SIGINT_handler);
+}
+
+void plotChart(std::vector<int> valuesToPlot, double maxX, double step) 
+{
+  Gnuplot gp;
+
+  int i=0;
+  std::vector<std::pair<double, int > > dataToPlot;
+  for(double x=0; x<maxX; x+=step) 
+  {
+    double y = x*x*x;
+    i++;
+    dataToPlot.push_back(std::make_pair(x, valuesToPlot[i]));
+  }
+
+  gp << "set autoscale xy\n";
+  gp << "plot '-' with lines title 'cubic', '-' with points title 'circle'\n";
+  gp.send1d(dataToPlot);
 }
 
 
